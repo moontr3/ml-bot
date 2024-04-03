9b27aeaca747686531edfc3df5250057e05e638a
moontr3 <rodionkovalkovskii@gmail.com>
2024-04-03 23:42:10 +0300

    level display

--- api.py
+++ api.py
@@ -3,9 +3,103 @@ from config import *
 import json
 import os
 from log import *
+from copy import deepcopy
 
 
-# user
+
+# user and user-related classes
+
+class PollOption:
+    def __init__(self, name:str, users:List[int]):
+        '''
+        Represents an option in a poll.
+        '''
+        self.name: str = name
+        self.users: List[int] = users
+        
+
+class Poll:
+    def __init__(self, data:dict):
+        '''
+        Represents a poll
+        '''
+        self.data: dict = data
+
+        self.channel_id: int = data['channel_id']
+        self.end_time: float = data['end_time']
+        self.duration: float = data['duration']
+        self.text: str = data['text']
+
+        self.options: List[PollOption] = [
+            PollOption(i, data['options'][i]) for i in data['options']
+        ]
+
+    def to_dict(self) -> dict:
+        '''
+        Converts the poll to a dict.
+        '''
+        return {
+            'channel_id': self.channel_id,
+            'end_time': self.end_time,
+            'duration': self.duration,
+            'text': self.text,
+            'options': {i.name: i.users for i in self.options}
+        }
+
+
+class Reminder:
+    def __init__(self, data:dict):
+        '''
+        Represents a reminder.
+        '''
+        self.message_id: int = data['id']
+        self.channel_id: int = data['channel_id']
+        self.end_time: float = data['end_time']
+        self.duration: float = data['duration']
+        self.text: str = data['text']
+        self.jump_url: str = data['jump_url']
+
+    def to_dict(self) -> dict:
+        '''
+        Converts the reminder to a dictionary.
+        '''
+        return {
+            "id": self.message_id,
+            "channel_id": self.channel_id,
+            "end_time": self.end_time,
+            "duration": self.duration,
+            "text": self.text,
+            "jump_url": self.jump_url
+        }
+
+
+class XP:
+    def __init__(self, xp:int):
+        '''
+        User experience points, level, rank, etc.
+        '''
+        self.xp: int = xp
+        self.reload_levels()
+
+
+    def reload_levels(self):
+        '''
+        Reloads levels and percentages.
+        '''
+        xp = deepcopy(self.xp)
+        xp_in_level = 1000
+        level = 1
+        while xp >= xp_in_level: 
+            level += 1
+            xp -= xp_in_level
+            xp_in_level += 500
+
+        self.level: int = level
+        self.level_data: any = config.LEVELS[min(level, len(config.LEVELS))-1]
+        self.level_xp: int = xp
+        self.level_max_xp: int = xp_in_level
+        self.level_percentage: float = xp/xp_in_level
+
 
 class User:
     def __init__(self, id:int, data:dict={}):
@@ -13,9 +107,18 @@ class User:
         Represents a user.
         '''
         self.id: int = id
-        self.data: dict = data
 
-        self.xp: int = data.get('xp', 0)
+        self.hah: bool = data.get('hah', False)
+        xp: int = data.get('xp', 0)
+        self.xp = XP(xp)
+        self.warns: int = data.get('warns', 0)
+        self.quarantine: float | None = data.get('quarantine', None)
+        self.polls: List[Poll] = [Poll(i) for i in data.get('polls', [])]
+        self.reminders: List[Reminder] = [Reminder(i) for i in data.get('reminders', [])]
+        self.tokens: Dict[int] = data.get('tokens', {})
+
+        self.token_dig_timeout: float = data.get('token_dig_timeout', 0.0)
+        self.games_timeout: float = data.get('games_timeout', 0.0)
 
     
     def to_dict(self) -> dict:
@@ -23,7 +126,15 @@ class User:
         Converts the class to a dictionary to store in the file.
         '''
         return {
-            "xp": self.xp
+            "xp": self.xp.xp,
+            "hah": self.hah,
+            "warns": self.warns,
+            "quarantine": self.quarantine,
+            "polls": [i.to_dict() for i in self.polls],
+            "reminders": [i.to_dict() for i in self.reminders],
+            "tokens": self.tokens,
+            "token_dig_timeout": self.token_dig_timeout,
+            "games_timeout": self.games_timeout
         }
 
 
@@ -74,6 +185,11 @@ class Manager:
         except:
             self.panic()
 
+        self.users = {int(id): User(int(id), data) for id, data in data['users'].items()}
+
+        # saving
+        self.commit()
+
 
     def commit(self):
         '''
@@ -84,9 +200,27 @@ class Manager:
         }
 
         # users
-        for i in self.users:    
-            data['users'][i] = i.to_dict()
+        for i in self.users:
+            data['users'][i] = self.users[i].to_dict()
 
         # saving
         with open(self.users_file, 'w', encoding='utf-8') as f:
-            json.dump(data, f)
+            json.dump(data, f, ensure_ascii=False)
+
+
+    def check_user(self, id:int):
+        '''
+        Checks if user exists in database. If not, creates one.
+        '''
+        if id in self.users:
+            return
+        
+        self.users[id] = User(id)
+
+
+    def get_user(self, id:int):
+        '''
+        Returns user by ID.
+        '''
+        self.check_user(id)
+        return self.users[id]
---
+++ cogs/leveling.py
@@ -0,0 +1,41 @@
+from discord.ext import commands
+import discord
+from data import *
+from log import *
+from typing import *
+from config import *
+import api
+
+
+# setup
+async def setup(bot: commands.Bot):
+
+    # xp command
+    @discord.app_commands.describe(
+        member='Участник сервера, у которого нужно узнать опыт.'
+    )
+    @discord.app_commands.command(
+        name='xp',
+        description='Показывает текущий опыт пользователя.'
+    )
+    async def slash_xp(ctx:discord.Interaction, member:discord.Member=None):
+        '''
+        Shows user XP level.
+        '''
+        if member == None:
+            member = ctx.user
+        acc: api.User = bot.mg.get_user(member.id)
+
+        log(f'{ctx.user.id} requested xp level of {member.id}')
+        role = ctx.guild.get_role(acc.xp.level_data)
+
+        desc = f'### {role.name.title()} ・ Уровень {acc.xp.level} ・ {acc.xp.xp} XP'\
+            f'\n**{acc.xp.level_xp}** / **{acc.xp.level_max_xp}** (**{int(acc.xp.level_percentage*100)}%**)'
+
+        embed = discord.Embed(
+            description=desc, color=role.color
+        )
+        embed.set_author(name=f'✨ Опыт {member.display_name}')
+        await ctx.response.send_message(embed=embed)
+
+    bot.tree.add_command(slash_xp)
