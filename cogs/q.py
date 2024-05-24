15c191a2fce76894662717612671922926926de1
moontr3 <rodionkovalkovskii@gmail.com>
2024-05-24 07:38:00 +0300

    add q

---
+++ cogs/q.py
@@ -0,0 +1,44 @@
+from discord.ext import commands
+import discord
+from data import *
+from log import *
+from typing import *
+from config import *
+import utils
+import datetime
+
+
+# setup
+async def setup(bot: commands.Bot):
+
+    # q command
+    @bot.command(
+        name='q',
+        description='q'
+    )
+    async def manual_q(ctx: commands.Context):
+        '''
+        Responds with Q.
+        '''
+        log(f'{ctx.author.id} invoked Q command')
+        
+        # fetching reference
+        try:
+            fetched = None if ctx.message.reference == None else\
+                await ctx.channel.fetch_message(ctx.message.reference.message_id)
+        except Exception as e:
+            log(f'Error while fetching reference to Q: {e}', level=WARNING)
+            fetched = None
+
+        # showing error
+        if not fetched:
+            embed = discord.Embed(
+                title='❌ Ошибка!', color=ERROR_C,
+                description=f'Надо __ответить на сообщение__.'
+            )
+            await ctx.reply(embed=embed)
+            return
+
+        # yaay success
+        await ctx.message.delete()
+        await fetched.add_reaction('🇶')
