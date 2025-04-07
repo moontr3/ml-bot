f35c5beb6fe41d302cbe7cd46f0fe1ae02b10420
n0n1m <97027325+n0n1m@users.noreply.github.com>
2025-04-07 12:00:08 +0300

    FAQ rework

--- cogs/faq.py
+++ cogs/faq.py
@@ -3,23 +3,9 @@ import discord
 from config import *
 
 async def setup(bot: commands.Bot):
-    # faq things
     question_list = bot.mg.data.get('faq', [])
 
-    class CustomSelect(discord.ui.Select):
-        async def callback(self, interaction:discord.Interaction):
-            index = int(self.values[0])
-            data = question_list[index]
-            
-            embed = discord.Embed(
-                title=data['name'],
-                description=data['contents'],
-                color=DEFAULT_C
-            )
-
-            await interaction.response.send_message(embed=embed, ephemeral=True)
-
-    # command itself
+    # command
     @bot.hybrid_command(
         name='faq',
         description='Показывает часто задаваемые вопросы.'
@@ -37,9 +23,25 @@ async def setup(bot: commands.Bot):
             options.append(discord.SelectOption(label=i['name'], value=str(c)))
 
         view = discord.ui.View(timeout=None)
-        view.add_item(CustomSelect(
+        view.add_item(discord.ui.Select(
+            custom_id='faq',
             options=options,
             placeholder='Выберите вопрос...'
         ))
 
         await ctx.reply(embed=embed, view=view)
+
+    # handling components
+    @bot.listen()
+    async def on_interaction(interaction:discord.Interaction):
+        if interaction.type == discord.InteractionType.component and interaction.data['custom_id'] == 'faq':
+            index = int(interaction.data['values'][0])
+            data = question_list[index]
+            
+            embed = discord.Embed(
+                title=data['name'],
+                description=data['contents'],
+                color=DEFAULT_C
+            )
+
+            await interaction.response.send_message(embed=embed, ephemeral=True)
