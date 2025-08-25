3526997b6c9a98eda0436f8529f16510c2efb2a4
moontr3 <rodionkovalkovskii@gmail.com>
2025-08-25 20:13:44 +0300

    better antispam

--- cogs/protection.py
+++ cogs/protection.py
@@ -1,5 +1,7 @@
+import aiohttp
 from discord.ext import commands
 import discord
+import api
 from log import *
 from typing import *
 from config import *
@@ -13,29 +15,71 @@ async def setup(bot: commands.Bot):
 
     @bot.listen()
     async def on_message(message: discord.Message):
+        if message.guild.id != GUILD_ID:
+            return
+        
+        # resolving interation author
+        user = message.author
+
+        if message.interaction_metadata:
+            orig_user = message.interaction_metadata.user
+
+            user = message.guild.get_member(orig_user.id)
+            if user == None:
+                user = await message.guild.fetch_member(orig_user.id)
+
+        botuser: api.User = bot.mg.get_user(user.id)
+
+        # checking for violations
+        if isinstance(user, discord.User):
+            return
+        
+        if user.get_role(IMBA_ROLE) or botuser.xp.xp > XP_THRESHOLD:
+            return
+        
+        reason = ''
+        
+        if len(message.mentions) > 3:
+            reason = f'・ {len(message.mentions)} пингов в сообщ\n'
+
+        if 'discord.gg/' in message.content.lower():
+            reason = f'・ Ссылка на сервер\n'
+
         if message.author.id in PIDORAS_ID:
-            if message.interaction_metadata:
-                user = message.interaction_metadata.user
-                try:
-                    user = message.guild.get_member(user.id)
-                    if user == None:
-                        user = await message.guild.fetch_member(user.id)
+            reason = f'・ Использование {message.author.name}\n'
+            
+        if reason:
+            log(f'Reason to ban {user.name} {user.id} - {reason}')
 
-                    if user.is_timed_out():
-                        await message.delete()
-                        return
-                    
-                    # timing out user
-                    if message.guild.get_role(IMBA_ROLE) in user.roles:
-                        length = datetime.timedelta(minutes=1)
-                    else:
-                        length = datetime.timedelta(days=1)
-
-                    await user.timeout(length, reason=f'Using {message.author.name}')
-                    log(f'Timed out user {user.id} for using {message.author.name} ({length.seconds//60} mins)')
-
-                except Exception as e:
-                    log(f'Unable to timeout user {user.id} for using {message.author.name}: {e}', level=ERROR)
-
-            else:
+            # deleting message
+            try:
                 await message.delete()
+            except Exception as e:
+                log('Error deleting message: ' + str(e), level=ERROR)
+
+            # banning user
+            try:
+                await user.ban(
+                    reason=f'Автор: {message.author.name} - {message.author.id} {reason}',
+                    delete_message_days=0
+                )
+            except Exception as e:
+                log('Error banning user: ' + str(e), level=ERROR)
+
+            # sending log message
+            try:
+                session = aiohttp.ClientSession()
+                webhook = discord.Webhook.from_url(bot.WEBHOOK, session=session)
+
+                embed = discord.Embed(color=3092790)
+
+                embed.add_field(name='Отправитель', value=f'`{user.name}` ・ {user.id}', inline=False)
+                if message.author != user:
+                    embed.add_field(name='Бот', value=f'`{message.author.name}` ・ {message.author.id}', inline=False)
+                embed.add_field(name='Причина', value=reason, inline=False)
+
+                await webhook.send(content=message.content, embed=embed, username='Удалён спам', avatar_url=WARN_IMAGE)
+                await session.close()
+
+            except Exception as e:
+                log('Error sending report to logs: ' + str(e), level=ERROR)
